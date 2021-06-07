824cfab76a67bc303e5e962532b313930d62c5e5
Bitardas <85475937+Bitardas@users.noreply.github.com>
2021-06-07 09:56:25 +0400

    Add files via upload

---
+++ Lab_4_API/app.js
@@ -0,0 +1,7 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 51.54580341692316, lng: 46.03534885722342 },
+        scrollwheel: true,
+        zoom: 8,
+    });
+}
