29fd4af5fe6e66f7f8e2c72126701abe1bb35918
Bitardas <85475937+Bitardas@users.noreply.github.com>
2021-06-07 17:31:06 +0400

    Delete Lab_4_API directory

--- Lab_4_API/app.js
+++
@@ -1,7 +0,0 @@
-function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 51.54580341692316, lng: 46.03534885722342 },
-        scrollwheel: true,
-        zoom: 8,
-    });
-}
